21b9eee9b699f25e9142a65efecd4bb8c304b612
Jen Strong <jenniferastrong@gmail.com>
2018-05-08 15:55:54 +0100

    Made directory interactive

--- directory.rb
+++ directory.rb
@@ -1,3 +1,20 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  students = []
+  name = gets.chomp
+
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
+
+
 students = [
   {name: "Dr. Hannibal Lecter", cohort: :november},
   {name: "Darth Vader", cohort: :november},
@@ -27,6 +44,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+students = input_students
 print_header
 print_student(students)
 print_footer(students)
